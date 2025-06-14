7792102ddcfe563b771090aafc371805428ec6e2
unknown <mohammad123alhalabi123+2@gmail.com>
2025-06-14 20:27:04 +0300

    Initial commit: Project setup with basic configuration files

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@testing-library|@babel)/)'
+  ]
+}; 
